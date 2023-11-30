61c07e90e15bf34020aff7a5f408dbd051017e88
ahmadwaryah <144982893+ahmadwaryah@users.noreply.github.com>
2023-11-30 20:03:26 +0500

    Add files via upload

---
+++ lab task.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+//Task 01;
+//int main() {
+//int x;
+//do
+//{
+//cout<<"Enter a number: ";
+//cin>>x;
+//}
+//while(x>0);
+//return 0;
+//}
+//Task 02:
+//int main() {
+// char choice;
+// float a, b, result;
+// do {
+// cout << "Enter the first number ";
+// cin >> a;
+// cout << "Enter the second number ";
+// cin >> b;
+// cout << "Enter the operator that you want to use, eg: (+, -, *, /): ";
+// cin >> choice;
+//switch (choice) {
+// case '+':
+// result = a + b;
+// break;
+// case '-':
+// result = a - b;
+// break;
+// case '*':
+// result = a * b;
+// break;
+// case '/':
+// result = a / b;
+// break;
+// default:
+// cout << "This is an Invalid operator";
+// continue;
+// }
+// cout << "Result: " << result << endl;
+// cout << "Enter 'R' to perform another calculation, or 'N' to quit: ";
+// cin >> choice;
+// } while (choice != 'N');
+// return 0;
+//}
+//
+
+
+
+
+
+//Task 03:
+//Part A:
+//int main() {
+//int x,c=0;
+//int n=0;
+//while(n<=100)
+//{
+//x=n+c;
+//c=x;
+// n=n+2;
+//}
+//cout<<x<<" "; 
+//return 0;
+//}
+
+
+//Part B:
+//int main() {
+//int x,c;
+//int n=1;
+//while(n<=100){
+//x=n*n;
+//c+=x;
+//n++;
+//}
+//cout<<"The sum of all squares between 1 and 100 is: "<<c;
+//return 0;}
+
+//Task 04:
+//PArt A:
+//int main() {
+//double x=2,r,v;
+//int n=0;
+//while(n<=20){
+//r=pow(x,n);
+//cout<<"2^"<<n<<" = "<<r<<endl;
+//n++;
+//}
+//return 0;}
+
+//Part B:
+//int main(){
+//int a,b,c=0,s;
+//cout<<"Enter the starting odd number: ";
+//cin>>a;
+//cout<<"Enter the ending odd number: ";
+//cin>>b;
+//int n=a;
+//while(n<=b)
+//{
+// s=n+c;
+//c=s; 
+// n=n+2;
+// 
+//}
+//cout<<s<<endl;
+//return 0;}
